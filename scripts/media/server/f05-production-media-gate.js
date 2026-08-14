b9339e50325bd6f8ea8154a514b3a81636d36a49
vvipautoparts-blip <294954557+vvipautoparts-blip@users.noreply.github.com>
2026-08-14 21:39:07 +0300

    feat(f05): bind production request to derivative gate

---
+++ scripts/media/server/f05-production-media-gate.js
@@ -0,0 +1,18 @@
+'use strict';
+const { createProductionMediaPorts, validateMediaRequestEnvelope } = require('./f05-production-media-ports.js');
+const { verifyAndRewriteCandidate } = require('./f05-derivative-gate.js');
+
+function fail(){const e=new Error('media_request_invalid');e.code='media_request_invalid';throw e;}
+function createProductionMediaGate(deps){
+  const ports=createProductionMediaPorts(deps);
+  return Object.freeze({
+    async handle(input){
+      if(!input||!(input.candidateBytes instanceof Uint8Array)||!input.request)fail();
+      const admitted=validateMediaRequestEnvelope(input.request);
+      if(input.candidateBytes.length<1||input.candidateBytes.length>admitted.maxCandidateBytes||input.request.contentLength<input.candidateBytes.length)fail();
+      return verifyAndRewriteCandidate({actor:input.actor,adScope:input.adScope,candidateBytes:input.candidateBytes,mediaPassport:input.mediaPassport},ports);
+    }
+  });
+}
+exports.createProductionMediaGate=createProductionMediaGate;
+Object.freeze(module.exports);
